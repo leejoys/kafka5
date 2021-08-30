5860ab1cebed46b3d011a229f5ec7e0fe108b2e0
leejoys <44557202+leejoys@users.noreply.github.com>
2021-08-30 03:16:49 +0300

    client main

--- main.go
+++ main.go
@@ -9,6 +9,34 @@
 
 package main
 
+import (
+	"errors"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type clientLocal struct {
+	Reader *kafka.Reader
+	Writer *kafka.Writer
+}
+
+func New(brokers []string, topic string) (*clientLocal, error) {
+	if len(brokers) == 0 || brokers[0] == "" || topic == "" {
+		return nil, errors.New("client params is not enough")
+	}
+	c := clientLocal{}
+
+	c.Reader = kafka.NewReader(kafka.ReaderConfig{
+		Brokers: brokers,
+		Topic:   topic})
+
+	c.Writer = &kafka.Writer{
+		Addr:  kafka.TCP(brokers[0]),
+		Topic: topic,
+	}
+	return &c, nil
+}
+
 func main() {
 
 }
