f953fb01beda479ce055cf4eede33a4c0c11ef23
leejoys <44557202+leejoys@users.noreply.github.com>
2021-08-30 04:40:27 +0300

    done

--- main.go
+++ main.go
@@ -10,7 +10,11 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"log"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -20,15 +24,17 @@ type clientLocal struct {
 	Writer *kafka.Writer
 }
 
-func New(brokers []string, topic string) (*clientLocal, error) {
-	if len(brokers) == 0 || brokers[0] == "" || topic == "" {
+func New(brokers []string, topic string, groupID string) (*clientLocal, error) {
+	if len(brokers) == 0 || brokers[0] == "" || topic == "" || groupID == "" {
 		return nil, errors.New("client params is not enough")
 	}
 	c := clientLocal{}
 
 	c.Reader = kafka.NewReader(kafka.ReaderConfig{
 		Brokers: brokers,
-		Topic:   topic})
+		Topic:   topic,
+		GroupID: groupID,
+	})
 
 	c.Writer = &kafka.Writer{
 		Addr:  kafka.TCP(brokers[0]),
@@ -37,6 +43,47 @@ func New(brokers []string, topic string) (*clientLocal, error) {
 	return &c, nil
 }
 
-func main() {
+func (c clientLocal) sendMsgs(msg kafka.Message) error {
+	err := c.Writer.WriteMessages(context.Background(), msg)
+	return err
+}
 
+func (c clientLocal) getMsgs() (*kafka.Message, error) {
+	msg, err := c.Reader.FetchMessage(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	err = c.Reader.CommitMessages(context.Background(), msg)
+	return &msg, err
+}
+
+func main() {
+	kfk, err := New([]string{"localhost:9093"}, "test", "test-group")
+	if err != nil {
+		log.Fatalf("Client constructor error: %s", err)
+	}
+
+	go func() {
+		for {
+			var msg kafka.Message
+			msg.Value = []byte(time.Now().String())
+			err := kfk.sendMsgs(msg)
+			if err != nil {
+				log.Fatalf("Writer error: %s", err)
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			msg, err := kfk.getMsgs()
+			if err != nil {
+				log.Fatalf("Reader error: %s", err)
+			}
+			fmt.Println(string(msg.Value))
+			time.Sleep(time.Second)
+		}
+	}()
+	time.Sleep(time.Second * 20)
 }
